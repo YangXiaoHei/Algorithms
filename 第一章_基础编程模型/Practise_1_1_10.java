a2019ef5f0a52dcf1f5cc7a059834c0e7621a3c4
yangxiaohei <yangxiaohei321123@163.com>
2017-10-23 12:13:17 +0800

    modify 1.1.10

--- 第一章_基础编程模型/Practise_1_1_10.java
+++ 第一章_基础编程模型/Practise_1_1_10.java
@@ -4,8 +4,8 @@ import edu.princeton.cs.algs4.*;
 
 public class Practise_1_1_10 {
 	public static void main(String[] args) {
-		int[] a; // 没有为 a 分配内存
-		for(int i = 0; i < 10; i++) 
-			a[i] = i * i;
+//		int[] a; // 没有为 a 分配内存
+//		for(int i = 0; i < 10; i++) 
+//			a[i] = i * i;
 	}
 }
