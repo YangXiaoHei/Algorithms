1cc3f6ed724077ac84af433094ff631385162cd8
yangxiaohei <yangxiaohei321123@163.com>
2017-11-03 00:05:39 +0800

    1.4.2

--- 第一章_算法分析/Practise_1_4_02.java
+++ 第一章_算法分析/Practise_1_4_02.java
@@ -4,19 +4,36 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Practise_1_4_02 {
 	static class ThreeSum {
-		public static int count(int[] a) {
+		/*
+		 * 正确处理溢出
+		 */
+		public static int correctCount(int[] a) {
 			int N = a.length;
 			int cnt = 0;
 			for (int i = 0; i < N; i++)
 				for (int j = i + 1; j < N; j++)
 					for (int k = j + 1; k < N; k++)
-						if ((long)(a[i] + a[j] + a[k]) == 0)
+						if ((long)a[i] + a[j] + a[k] == 0)
+							cnt++;
+			return cnt;
+		}
+		/*
+		 * 不处理溢出
+		 */
+		public static int buggyCount(int[] a) {
+			int N = a.length;
+			int cnt = 0;
+			for (int i = 0; i < N; i++)
+				for (int j = i + 1; j < N; j++)
+					for (int k = j + 1; k < N; k++)
+						if (a[i] + a[j] + a[k] == 0)
 							cnt++;
 			return cnt;
 		}
 	}
 	public static void main(String[] args) {
-		int[] arr = new int[] { Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, 1};
-		StdOut.println(ThreeSum.count(arr));
+		int[] arr = new int[] { Integer.MIN_VALUE, Integer.MIN_VALUE, 0};
+		StdOut.println("正确处理溢出 ：" + ThreeSum.correctCount(arr));
+		StdOut.println("不处理溢出 : " + ThreeSum.buggyCount(arr));
 	}
 }
