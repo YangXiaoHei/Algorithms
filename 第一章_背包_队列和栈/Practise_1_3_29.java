c01dae67d5c7fb598dd1aaa72f34768f01689f55
yangxiaohei <yangxiaohei321123@163.com>
2017-11-12 20:38:19 +0800

    1.3.29

--- 第一章_背包_队列和栈/Practise_1_3_29.java
+++ 第一章_背包_队列和栈/Practise_1_3_29.java
@@ -6,68 +6,59 @@ public class Practise_1_3_29 {
 	static class Node<T> {
 		T item;
 		Node<T> next;
+		Node(T item) { this(item, null); }
 		Node(T item, Node<T> next) {
 			this.item = item;
 			this.next = next;
 		}
-		Node(T item) { this(item, null); }
 	}
 	static class CircularList<T> {
-		private Node<T> head = null;
+		private Node<T> tail = null, head = null;
 		private int size;
+		boolean isEmpty() { return size == 0; }
 		void insertLast(T item) {
-			if (head == null) {
+			if (isEmpty()) {
 				Node<T> node = new Node<T>(item);
 				node.next = node;
-				head = node;
+				head = tail = node;
 			} else {
-				Node<T> tmp = head;
-				for(int i = 1; i < size;i ++)
-					tmp = tmp.next;
 				Node<T> node = new Node<T>(item);
-				tmp.next = node;
+				tail.next = node;
 				node.next = head;
+				tail = node;
 			}
 			size++;
 		}
 		T removeFirst() {
-			if (size == 0) 
+			if (isEmpty()) 
 				throw new RuntimeException("empty circular list");
-			T del = head.item;
+			T del = tail.next.item;
+			tail.next.item = null;
 			if (size == 1) {
-				head = null;
+				head = tail = null;
 			} else {
-				head.item = null;
 				head = head.next;
-				Node<T> tmp = head;
-				for(int i = 1; i < size - 1; i++)
-					tmp = tmp.next;
-				tmp.next = head;
+				tail.next = tail.next.next;
 			}
 			size--;
 			return del;
 		}
 		public String toString() {
+			if (isEmpty()) return "empty list";
+			Node<T> cur = head;
 			StringBuilder sb = new StringBuilder();
-			Node<T> node = head;
-			for(int i = 0; i < size - 1; i++) {
-				sb.append(node.item + " -> ");
-				node = node.next;
+			while (cur != tail) {
+				sb.append(cur.item + " -> ");
+				cur = cur.next;
 			}
-			sb.append(node.item);
+			sb.append(cur.item);
 			return sb.toString();
 		}
 	}
 	static class Queue<Q> extends CircularList<Q> {
-		void enqueue(Q item) {
-			insertLast(item);
-		}
-		Q dequeue() {
-			return removeFirst();
-		}
-		public String toString() {
-			return super.toString();
-		}
+		void enqueue(Q item) { insertLast(item); }
+		Q dequeue() { return removeFirst(); }
+		public String toString() { return super.toString(); }
 	}
 	public static void main(String[] args) {
 		Queue<Integer> queue = new Queue<Integer>();
