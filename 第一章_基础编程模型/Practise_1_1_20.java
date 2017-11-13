9459016f9403dc34e6cd582c0f75ee13c6c48c2f
yangxiaohei <yangxiaohei321123@163.com>
2017-11-14 00:06:58 +0800

    1.1.20

--- 第一章_基础编程模型/Practise_1_1_20.java
+++ 第一章_基础编程模型/Practise_1_1_20.java
@@ -4,7 +4,7 @@ import edu.princeton.cs.algs4.*;
 
 public class Practise_1_1_20 {
 	/*
-	 * ln(N!)
+	 * 方法一
 	 */
 	public static double lnN(int N) {
 		return Math.log(recursive(N));
@@ -13,10 +13,21 @@ public class Practise_1_1_20 {
 		if (N == 1 || N == 0) return 1;
 		return recursive(N - 1) * N;
 	}
+	/*
+	 * 方法二
+	 */
+	public static double lnN2(int N) {
+		if (N == 1 || N == 0) return 0;
+		return lnN2(N - 1) + Math.log(N);
+	}
 	public static void main(String[] args) {
 		int N = 10;
-		for(int i = 0; i < N; i++)
+		for(int i = 0; i < N; i++) 
 			StdOut.println(lnN(i));
+		
+		StdOut.println();
+		for (int i = 0; i < N; i++)
+			StdOut.println(lnN2(i));
 	}
 	// output :
 	/*
@@ -30,6 +41,17 @@ public class Practise_1_1_20 {
 		8.525161361065415
 		10.60460290274525
 		12.801827480081469
+		
+		0.0
+		0.0
+		0.6931471805599453
+		1.791759469228055
+		3.1780538303479453
+		4.787491742782046
+		6.579251212010101
+		8.525161361065415
+		10.60460290274525
+		12.80182748008147
 
 	 */
 }
