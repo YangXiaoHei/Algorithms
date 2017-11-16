15c793ed846ce1f6f1acdb33cfc2effe798771f3
yangxiaohei <yangxiaohei321123@163.com>
2017-11-16 22:39:05 +0800

    1.5.16

--- 第一章_基础编程模型/Practise_1_1_31.java
+++ 第一章_基础编程模型/Practise_1_1_31.java
@@ -90,7 +90,7 @@ public class Practise_1_1_31 {
 	}
 	
 	public static void main(String[] args) {
-		draw(10 /* 一万个点 */, 0.8 /* 0.3的概率会连线 */);
+		draw(100, 0.1);
 	}
 	// output : execute to see graph drawing
 }
