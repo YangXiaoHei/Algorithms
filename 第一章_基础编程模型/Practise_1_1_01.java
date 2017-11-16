8ada038561ade2f44f33ed97d276ce0bf91ba681
yangxiaohei <yangxiaohei321123@163.com>
2017-11-17 00:02:44 +0800

    1.1.1

--- 第一章_基础编程模型/Practise_1_1_01.java
+++ 第一章_基础编程模型/Practise_1_1_01.java
@@ -4,23 +4,24 @@ import edu.princeton.cs.algs4.*;
 
 public class Practise_1_1_01 {
 	public static void main(String[] args) {
-		/*
-		 * 整型相除，向0舍入
-		 */
-		StdOut.println( (0 + 15) / 2 );
-		/*
-		 * 字面值是双精度
-		 */
-		StdOut.println( 2.0e-6 * 100000000.1);
-		/*
-		 *  && 优先级高于 ||
-		 */
-		StdOut.println( true && false || true && true );
+	    /*
+	     * 整型相除，向0舍入
+         */
+	    StdOut.println( (0 + 15) / 2 );
+	    /*
+	     * 字面值是双精度
+	     */
+	    StdOut.println( 2.0e-6 * 100000000.1);
+	    /*
+	     *  && 优先级高于 ||
+	     */
+	    StdOut.println( true && false || true && true );
 	}
 	// output :
 	/*
 	 * 	7
-		200.0000002
-		true
+        200.0000002
+        true
+
 	 */
 }
