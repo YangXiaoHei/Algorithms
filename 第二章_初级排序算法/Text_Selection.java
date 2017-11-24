1b94abbc5c3e8b5f29330e24fd3cce41c21f78bc
yangxiaohei <yangxiaohei321123@163.com>
2017-11-24 23:16:11 +0800

    复习

--- 第二章_初级排序算法/Text_Selection.java
+++ 第二章_初级排序算法/Text_Selection.java
@@ -5,6 +5,22 @@ import java.util.Arrays;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
+/*
+ * 选择排序 :
+ * 
+ * 假设 i = 0 是 1~N-1 中最小的元素，在 1~N-1 中依次与 i = 0 比较，找出一个最小者，并与 i = 0 交换位置
+ * 假设 i = 1 是 2~N-1 中最小的元素，在 2~N-1 中依次与 i = 1 比较，找出一个最小者，并与 i = 1 交换位置
+ * 假设 i = 2 是 3~N-1 中最小的元素，在 3~N-1 中依次与 i = 2 比较，找出一个最小者，并与 i = 2 交换位置
+ * ...
+ * 当 i = N-1 时，排序终止
+ * 
+ * 可以看出，选择排序的比较总次数为 N-1 + N-2 + N-3 + ... + 1 = N(N-1)/2 ~ N^2/2
+ * 
+ * 元素交换的次数为 N 次
+ * 
+ * 可以看出 选择排序是一个增长率为平方级别的算法
+ * 
+ */
 public class Text_Selection {
     public static void sort(Comparable<?>[] a) {
         for (int i = 0; i < a.length; i++) {
