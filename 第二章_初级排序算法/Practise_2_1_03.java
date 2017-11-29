126a2083837a1fe1547a2d2adc6271b28516ec89
yangxiaohei <yangxiaohei321123@163.com>
2017-11-29 16:28:40 +0800

    2.1.2 ~ 2.1.4

---
+++ 第二章_初级排序算法/Practise_2_1_03.java
@@ -0,0 +1,11 @@
+package 第二章_初级排序算法;
+
+public class Practise_2_1_03 {
+    public static void main(String[] args) {
+        /*
+         * 一个元素完全相等的数组
+         * 
+         * 所有比较都会失败
+         */
+    }
+}
