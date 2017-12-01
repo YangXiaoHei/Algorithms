d99f9522e6fbb9e5c41646f920839616c90391b9
yangxiaohei <yangxiaohei321123@163.com>
2017-12-01 11:24:57 +0800

    2.1.17

---
+++ 第二章_初级排序算法/Practise_2_1_17.java
@@ -0,0 +1,63 @@
+package 第二章_初级排序算法;
+
+import static 第二章_初级排序算法.Text_Array.*;
+
+import java.awt.Color;
+import java.util.concurrent.TimeUnit;
+
+import edu.princeton.cs.algs4.*;
+
+public class Practise_2_1_17 {
+    public static void draw(Double[] a, int markIndex1, int markIndex2) {
+        StdDraw.clear();
+        StdDraw.setXscale(-1, a.length);
+        StdDraw.setYscale(-0.5, 1.5);
+        for (int i = 0; i < a.length; i++) {
+            double x = i;
+            double y = a[i] / 2.0;
+            double rw = (1 - 0.3) / 2;
+            double rh = a[i] / 2.0;
+            if (i == markIndex1 || i == markIndex2)
+                StdDraw.setPenColor(Color.RED);
+            else
+                StdDraw.setPenColor(Color.BLACK);
+            StdDraw.filledRectangle(x, y, rw, rh);
+        }
+    }
+    public static void insertion(Double[] a) {
+        for (int i = 1; i < a.length; i++)
+            for (int j = i; j > 0 && a[j].compareTo(a[j - 1]) < 0; j--) {
+                draw(a,j, j - 1);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(200);
+                } catch (Exception e) {}
+                Double t = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = t;
+                draw(a,j, j - 1);
+            }
+    }
+    public static void shell(Double[] a) {
+        int N = a.length, h = 1;
+        while (h < N / 3) h = 3 * h + 1;
+        while (h >= 1) {
+            for (int i = h; i < N; i++) 
+                for (int j = i; j >= h && a[j].compareTo(a[j - h]) < 0; j -= h) {
+                    draw(a, j, j - h);
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(200);
+                    } catch (Exception e) {}
+                    Double t = a[j];
+                    a[j] = a[j - h];
+                    a[j - h] = t;
+                    draw(a, j, j - h);
+                }
+            h /= 3;
+        }
+    }
+    public static void main(String[] args) {
+        Double[] a = DoubleRandom_size(50);
+//        insertion(a);
+        shell(a);
+    }
+}
