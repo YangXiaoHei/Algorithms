edf6b2f3fdb4510713c42259377fe986cf303880
yangxiaohei <yangxiaohei321123@163.com>
2017-12-02 21:28:19 +0800

    2.1.32

---
+++ 第二章_初级排序算法/Practise_2_1_32.java
@@ -0,0 +1,84 @@
+package 第二章_初级排序算法;
+
+import static 第二章_初级排序算法.Text_Array.*;
+import java.awt.Color;
+import edu.princeton.cs.algs4.*;
+
+public class Practise_2_1_32 {
+    public static double insertion(int[] arr) {
+        double avrg = 0;
+        int[] a = null;
+        for (int loop = 0; loop < 3; loop++) {
+            a = intCopy_arr(arr);
+            Stopwatch timer = new Stopwatch();
+            int N = a.length;
+            for (int i = 1; i < N; i++) {
+                int t = a[i], j;
+                for (j = i - 1; j >= 0 && t < a[j]; j--)
+                    a[j + 1] = a[j];
+                a[j + 1] = t;
+            }
+            avrg += timer.elapsedTime();
+        }
+        return avrg / 3;
+    }
+    public static double selection(int[] arr) {
+        double avrg = 0;
+        int[] a = null;
+        for (int loop = 0; loop < 3; loop++) {
+            a = intCopy_arr(arr);
+            Stopwatch timer = new Stopwatch();
+            int N = a.length;
+            for (int i = 0; i < N; i++) {
+                int min = i;
+                for (int j = i + 1; j < N; j++)
+                    if (a[j] < a[min]) min = j;
+                int t = a[min];
+                a[min] = a[i];
+                a[i] = t;
+            }
+            avrg += timer.elapsedTime();
+        }
+        return avrg / 3;
+    }
+    public static double shell(int[] arr) {
+        double avrg = 0;
+        int[] a = null;
+        for (int loop = 0; loop < 3; loop++) {
+            a = intCopy_arr(arr);
+            Stopwatch timer = new Stopwatch();
+            int N = a.length, h = 1;
+            while (h < N / 3) h = 3 * h + 1;
+            while (h >= 1) {
+                for (int i = h; i < N; i++) {
+                    int t = a[i], j;
+                    for (j = i - h; j >= 0 && t < a[j]; j -= h) 
+                        a[j + h] = a[j];
+                    a[j + h] = t;
+                }
+                h /= 3;
+            }
+            avrg += timer.elapsedTime();
+        }
+        return avrg / 3;
+    }
+    public static void draw(int N) {
+        StdDraw.setXscale(N - 10, 80000);
+        StdDraw.setYscale(-1, 3);
+        StdDraw.setPenRadius(.003);
+        for (int i = N; i < 80000; i += 500) {
+            int[] a = intRandom_size_bounds(i, 0, i * 10);
+            for (int k = 0; k < 1; k++) {
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.point(i, shell(a));
+                StdDraw.setPenColor(Color.RED);
+                StdDraw.point(i, insertion(a));
+                StdDraw.setPenColor(Color.BLUE);
+                StdDraw.point(i, selection(a));
+            }
+        }
+    }
+    public static void main(String[] args) {
+        draw(1000);
+    }
+}
