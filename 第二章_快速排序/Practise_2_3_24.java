ab3430793455e7cf1303cfd28071ea1886e54922
yangxiaohei <yangxiaohei321123@163.com>
2017-12-17 18:14:16 +0800

    2.3.24

--- 第二章_快速排序/Practise_2_3_24.java
+++ 第二章_快速排序/Practise_2_3_24.java
@@ -4,6 +4,45 @@ import edu.princeton.cs.algs4.*;
 import static 第二章_初级排序算法.Text_Array.*;
 
 public class Practise_2_3_24 {
+    public static double __quick(int[] a) {
+        Stopwatch timer = new Stopwatch();
+        __quick(a, 0, a.length - 1);
+        return timer.elapsedTime();
+    }
+    private static void __quick(int[] a, int lo, int hi) {
+        if (hi - lo + 1 < 47) {
+            for (int i = lo; i <= hi; i++) {
+                int t = a[i], j;
+                for (j = i - 1; j >= 0 && t < a[j]; j--)
+                    a[j + 1] = a[j];
+                a[j + 1] = t;
+            }
+            return;
+        }
+        int mid = (lo + hi) >>> 1;
+        if (a[mid] < a[lo]) exch(a, mid, lo);
+        if (a[hi] < a[lo])  exch(a, lo, hi);
+        if (a[mid] < a[hi]) exch(a, mid, hi);
+        
+        int i = lo - 1, j = hi, p = lo - 1, q = hi, v = a[hi];
+        while (true) {
+            while (a[++i] < v);
+            while (a[--j] > v);
+            if (i >= j) break;
+            exch(a, i, j);
+            if (a[i] == v) exch(a, ++p, i);
+            if (a[j] == v) exch(a, --q, j);
+        }
+        exch(a, i, hi);
+        int lt = i - 1, gt = i + 1, k = lo, m = hi - 1;
+        while (k <= p) exch(a, k++, lt--);
+        while (m >= q) exch(a, m--, gt++);
+        __quick(a, lo, lt);
+        __quick(a, gt, hi);
+    }
+    private static void exch(int[] a, int i, int j) {
+        int t = a[i]; a[i] = a[j]; a[j] = t;
+    }
     public static double _quick(int[] a) {
         Stopwatch timer = new Stopwatch();
         _quick(a, 0, a.length - 1);
@@ -65,23 +104,45 @@ public class Practise_2_3_24 {
         quick(a, j + 1, hi, pivots);  
     }
     public static void main(String[] args) {
-        int[] a = ints(0, 10000000);
+//        int[] a = ints(0, 4000000);
+//        int[] a = allSameInts(4000000, 1);
+        int[] a = intsVrg(4000000, 1, 2, 3, 4);
+//        int[] a = ascendInts(0, 4000000);
         int[] copy = intsCopy(a);
+        int[] copy1 = intsCopy(a);
         StdOut.printf("取样排序 : %.3f\n", quick(a));
         StdOut.printf("普通快排 : %.3f\n", _quick(copy));
+        StdOut.printf("优化快排 : %.3f\n", __quick(copy1));
         assert isSorted(a);
         assert isSorted(copy);
+        assert isSorted(copy1);
     }
     // output
     /*
-     *  取样排序 : 2.691
-        普通快排 : 2.500
-        
-        取样排序 : 2.635
-        普通快排 : 2.495
-        
-        取样排序 : 2.787
-        普通快排 : 2.278
+     *
+     * 乱序无重复
+     * 
+     * 取样排序 : 0.980
+       普通快排 : 1.052
+       优化快排 : 0.827
+       
+       全部都相同
+       
+       取样排序 : 0.390
+       普通快排 : 0.328
+       优化快排 : 0.115
+       
+       大量重复元素
+       
+       取样排序 : 0.594
+       普通快排 : 0.552
+       优化快排 : 0.259
+       
+       升序
+       栈溢出
+       栈溢出
+       优化快排 : 0.145
+
 
      */
 }
