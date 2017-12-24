a48e15b9ad440d91b9f5328f1e077983d773dd4a
yangxiaohei <yangxiaohei321123@163.com>
2017-12-24 11:13:41 +0800

    2.4.10

---
+++ 第二章_优先队列/Practise_2_4_10.java
@@ -0,0 +1,17 @@
+package 第二章_优先队列;
+
+public class Practise_2_4_10 {
+    public static void main(String[] args) {
+        /*
+         * 
+         * 设 p[k] 子结点为 SUB(k)  推理可知，0 -> 12 -> 3456 -> 7891011121314 -> ...      
+         *             
+         * SUB(k) = {左 : 2k+1, 右 : 2k+2}
+         * 
+         * 设 p[k] 父结点为 PAR(k)
+         *               
+         * PAR(k) = floor[(k-1)/2]
+         * 
+         */
+    }
+}
