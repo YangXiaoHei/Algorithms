6c4ca1796bc64fc38516491c661507925d9c8086
yangxiaohei <yangxiaohei321123@163.com>
2017-12-30 23:46:38 +0800

    2.4.26

--- 第二章_优先队列/Practise_2_4_26.java
+++ 第二章_优先队列/Practise_2_4_26.java
@@ -3,7 +3,6 @@ package 第二章_优先队列;
 import java.util.*;
 import static 第二章_初级排序算法.Text_Array.*;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
 
 public class Practise_2_4_26 {
     static class MaxPQ<Key extends Comparable<Key>> {
@@ -26,16 +25,15 @@ public class Practise_2_4_26 {
                 keys[k] = keys[k >> 1];
                 k >>= 1;
             }
-            StdOut.printf("k = %d\n", k);
             keys[k] = key;
         }
         public Key delMax() {
             if (isEmpty())
                 throw new NoSuchElementException("priority queue underflow");
             Key max = keys[1];
-            keys[1] = keys[size];
-            Key last = keys[size--];
-            int k = size;
+            keys[1] = keys[size--];
+            Key last = keys[1];
+            int k = 1;
             while ((k << 1) <= size) {
                 int j = k << 1;
                 if (keys[j].compareTo(keys[j + 1]) < 0) j++;
@@ -51,20 +49,122 @@ public class Practise_2_4_26 {
         }
         public String toString() {
             StringBuilder sb = new StringBuilder();
-            for (int i = 1; i <= size; i++)
+            for (int i = 0; i < keys.length; i++)
                 sb.append(keys[i] + " ");
             sb.append("\n");
             return sb.toString();
         }
     }
     public static void main(String[] args) {
-        Integer[] a = Integers(0, 10);
-        print(a);
+        Integer[] a = Integers(0, 100);
         MaxPQ<Integer> pq = new MaxPQ<Integer>();
-        for (int i = 0; i < 10; i++)
-            pq.insert(StdRandom.uniform(10));
-        StdOut.println(pq);
-//        while (!pq.isEmpty())
-//            StdOut.println(pq.delMax());
+        for (int i = 0; i < a.length; i++)
+            pq.insert(a[i]);
+        while (!pq.isEmpty())
+            StdOut.println(pq.delMax());
     }
+    // output
+    /*
+     *  100
+        99
+        98
+        97
+        96
+        95
+        94
+        93
+        92
+        91
+        90
+        89
+        88
+        87
+        86
+        85
+        84
+        83
+        82
+        81
+        80
+        79
+        78
+        77
+        76
+        75
+        74
+        73
+        72
+        71
+        70
+        69
+        68
+        67
+        66
+        65
+        64
+        63
+        62
+        61
+        60
+        59
+        58
+        57
+        56
+        55
+        54
+        53
+        52
+        51
+        50
+        49
+        48
+        47
+        46
+        45
+        44
+        43
+        42
+        41
+        40
+        39
+        38
+        37
+        36
+        35
+        34
+        33
+        32
+        31
+        30
+        29
+        28
+        27
+        26
+        25
+        24
+        23
+        22
+        21
+        20
+        19
+        18
+        17
+        16
+        15
+        14
+        13
+        12
+        11
+        10
+        9
+        8
+        7
+        6
+        5
+        4
+        3
+        2
+        1
+        0
+     */
 }
