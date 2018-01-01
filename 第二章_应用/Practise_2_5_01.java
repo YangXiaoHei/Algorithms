6bb0168d85449b04f5c3a47f9ddd78013cf53638
yangxiaohei <yangxiaohei321123@163.com>
2018-01-01 14:54:51 +0800

    2.5.1

---
+++ 第二章_应用/Practise_2_5_01.java
@@ -0,0 +1,11 @@
+package 第二章_应用;
+
+public class Practise_2_5_01 {
+    public static void main(String[] args) {
+        /*
+         * 如果内存地址相等，那么这两个字符串肯定相等，因此无需比较
+         * 这样避免了多余的比较操作
+         * 
+         */
+    }
+}
