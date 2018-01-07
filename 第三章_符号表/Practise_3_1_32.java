dc922103648ead37c20d54503b45ff47fa8fdb24
yangxiaohei <yangxiaohei321123@163.com>
2018-01-07 17:54:15 +0800

    3.1.32

---
+++ 第三章_符号表/Practise_3_1_32.java
@@ -0,0 +1,27 @@
+package 第三章_符号表;
+
+import static 第二章_初级排序算法.Text_Array.*;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Practise_3_1_32 {
+    public static void main(String[] args) {
+        Practise_3_1_26.ST<Integer, String> st = new Practise_3_1_26.ST<Integer, String>();
+        int N = 1000;
+        Integer[] ascend = ascendIntegers(0, N - 1);
+        Integer[] descend = descendIntegers(N - 1, 0);
+        Integer[] allSame = intToInteger(allSameInts(N, 5));
+        Integer[] twoValue = intToInteger(intsVrg(N, 1, 2));
+//        for (Integer i : ascend)
+//            st.put(i, "A");
+//        StdOut.println(st);
+//        for (Integer i : descend)
+//            st.put(i, "A");
+//        StdOut.println(st);
+//        for (Integer i : allSame)
+//            st.put(i, "A");
+//        StdOut.println(st);
+        for (Integer i : twoValue)
+            st.put(i, "A");
+        StdOut.println(st);
+    }
+}
