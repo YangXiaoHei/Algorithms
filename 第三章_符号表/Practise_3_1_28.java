c6337a81fc5bd29e8ea5460a87f89551006eac5c
yangxiaohei <yangxiaohei321123@163.com>
2018-01-07 17:47:26 +0800

    将有序符号表在键有序情况下的put操作改善至常数时间

---
+++ 第三章_符号表/Practise_3_1_28.java
@@ -0,0 +1,13 @@
+package 第三章_符号表;
+
+public class Practise_3_1_28 {
+    public static void main(String[] args) {
+        /*
+         * 在 put 里面加个判断，如果大于最大值，那么就不进行二分查找，
+         * 直接走扩容然后插入那一套
+         * 
+         * 请看 3.1.26
+         * 
+         */
+    }
+}
