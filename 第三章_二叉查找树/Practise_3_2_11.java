d76633a25ed75918180da2f70001a751cafda7f4
yangxiaohei <yangxiaohei321123@163.com>
2018-01-10 12:21:40 +0800

    N个键可组成不同二叉查找树形态

---
+++ 第三章_二叉查找树/Practise_3_2_11.java
@@ -0,0 +1,13 @@
+package 第三章_二叉查找树;
+
+public class Practise_3_2_11 {
+    public static void main(String[] args) {
+        /*
+         * 设 N 个结点可以组成 h(N) 个形状不同的二叉树
+         * 则 h(N) = C(2N, N) / (N + 1)
+         * 
+         * N 个键能构成的树最大高度为 N - 1，因此不可能构成高度为 N 的二叉树
+         * 
+         */
+    }
+}
