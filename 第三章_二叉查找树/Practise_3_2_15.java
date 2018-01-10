39673ed463cafa42b59de1744bd1d180e25c6d59
yangxiaohei <yangxiaohei321123@163.com>
2018-01-10 13:36:05 +0800

    3.2.15

---
+++ 第三章_二叉查找树/Practise_3_2_15.java
@@ -0,0 +1,44 @@
+package 第三章_二叉查找树;
+
+import java.util.LinkedList;
+public class Practise_3_2_15 {
+    public static void main(String[] args) {
+        /*
+         * floor("Q")  
+         * 
+         * E -> Q
+         * 
+         * select(5)
+         * 
+         * E -> Q
+         * 
+         * ceiling("Q")
+         * 
+         * E -> Q
+         * 
+         * rank("J")
+         * 
+         * E -> Q -> J
+         * 
+         * size("D", "T")
+         * 
+         * E -> D 
+         * 
+         * E -> Q -> T
+         * 
+         * keys("D", "T")
+         * 
+         * private void keys(Node n, LinkedList<K> list, K lo, K hi) {
+            if (n == null) return;
+            int cmplo = lo.compareTo(n.k);
+            int cmphi = n.k.compareTo(hi);
+            if (cmplo < 0) keys(n.left, list, lo, hi);
+            if (cmplo <= 0 && cmphi <= 0) list.add(n.k);
+            if (cmphi < 0) keys(n.right, list, lo, hi);
+        }
+           
+           E -> D -> E -> Q -> J -> J -> M -> Q -> T -> S -> S -> T
+        
+         */
+    }
+}
