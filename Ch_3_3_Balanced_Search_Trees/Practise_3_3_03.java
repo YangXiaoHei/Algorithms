c03bc18a44cec291544a8c4dad0f25488806577b
yangxiaohei <yangxiaohei321123@163.com>
2018-01-19 22:40:53 +0800

    构造高度为1的2-3树

---
+++ Ch_3_3_Balanced_Search_Trees/Practise_3_3_03.java
@@ -0,0 +1,18 @@
+package Ch_3_3_Balanced_Search_Trees;
+
+public class Practise_3_3_03 {
+    public static void main(String[] args) {
+        /*
+         * 高度为 1 的 2-3 树可以想象出
+         * 
+         * 最大可容纳结点个数为 8 个， 形如下列图像
+         * 
+         *                  xx
+         *              xx  xx  xx
+         *              
+         * https://github.com/YangXiaoHei/Algorithms_4/blob/master/FlowChart/Ch_3_3_Practise_3_3_03.png
+         * 从上述构造出的高度为 1 的 2-3 树可以得到
+         * 插入序列可以是  E S A H X C M (这里仅列举一种可能的)
+         */
+    }
+}
