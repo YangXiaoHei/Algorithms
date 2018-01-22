b660f21318f42b5aa06779d0fd15133bb42a9a0f
yangxiaohei <yangxiaohei321123@163.com>
2018-01-22 20:23:16 +0800

    3.3.9

---
+++ Ch_3_3_Balanced_Search_Trees/Practise_3_3_09.java
@@ -0,0 +1,11 @@
+package Ch_3_3_Balanced_Search_Trees;
+
+public class Practise_3_3_09 {
+    public static void main(String[] args) {
+        /*
+         * 1 不是红黑树，把红色摊平将其视作2-3树就可以看出
+         * 2 不满足中序遍历次序按照升序排列
+         * 3, 4 都是红黑树
+         */
+    }
+}
