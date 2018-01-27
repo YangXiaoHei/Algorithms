76ce8a3f1a0a542572490fa6371274b530c94279
yangxiaohei <yangxiaohei321123@163.com>
2018-01-27 16:56:24 +0800

    红黑树单调性

---
+++ Ch_3_3_Balanced_Search_Trees/Practise_3_3_13.java
@@ -0,0 +1,15 @@
+package Ch_3_3_Balanced_Search_Trees;
+
+public class Practise_3_3_13 {
+    public static void main(String[] args) {
+        /*
+         * 真
+         * 
+         * 每插入一个结点，树高要么保持不变，要么在叶子形成 3- 结点，首先分裂，然后将 2- 结点向上传递
+         * 当 2- 结点传递到根结点并形成 3- 结点时，树高加1
+         * 因此 每插入一个结点，树高要么不变，要么加 1
+         * 
+         * https://github.com/YangXiaoHei/Algorithms_4/blob/master/FlowChart/Ch_3_3_Practise_3_3_13.png
+         */
+    }
+}
