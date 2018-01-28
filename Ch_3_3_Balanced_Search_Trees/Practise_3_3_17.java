3dcdd6703a6b89299ac0c09a6474fcf2b8d7f586
yangxiaohei <yangxiaohei321123@163.com>
2018-01-28 20:54:01 +0800

    红黑树和二叉查找树对比

---
+++ Ch_3_3_Balanced_Search_Trees/Practise_3_3_17.java
@@ -0,0 +1,12 @@
+package Ch_3_3_Balanced_Search_Trees;
+
+public class Practise_3_3_17 {
+    public static void main(String[] args) {
+        /*
+         * 结点较少时形成的二叉查找树高度不一定大于红黑树，但内部路径长度一定小于红黑树
+         * 这代表随机命中查找所需的平均比较次数一定大于红黑树
+         * 
+         * https://github.com/YangXiaoHei/Algorithms_4/blob/master/FlowChart/Ch_3_3_Practise_3_3_17.png
+         */
+    }
+}
