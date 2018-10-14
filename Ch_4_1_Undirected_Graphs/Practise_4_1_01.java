ecd4b9169439897b474fb865753a9e3f85cb131e
yangxiaohei <yangxiaohei321123@163.com>
2018-10-15 00:37:42 +0800

    4.1.1 ~ 4.1.7

--- Ch_4_1_Undirected_Graphs/Practise_4_1_01.java
+++ Ch_4_1_Undirected_Graphs/Practise_4_1_01.java
@@ -1,15 +1,13 @@
 package Ch_4_1_Undirected_Graphs;
 
-/*
- * 一幅含有 V 个顶点且不含有平行边的图中至多含有多少条边？一幅含有
- * V 个顶点的连通图中至少含有多少条边？
- */
 public class Practise_4_1_01 {
     public static void main(String[] args) {
         /*
          * V(V-1)/2
          * 
          * V-1
+         * 
+         * 见 README.md
          */
     }
 }
---
+++ Ch_4_1_Undirected_Graphs/Practise_4_1_03.java
@@ -0,0 +1,5 @@
+package Ch_4_1_Undirected_Graphs;
+
+public class Practise_4_1_03 {
+
+}
