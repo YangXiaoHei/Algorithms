f8012a2ba50d514a43b2e44b26f3c30eb5d4ab46
yangxiaohei <yangxiaohei321123@163.com>
2018-10-26 13:13:13 +0800

    3 ~ 6

--- Ch_4_3_Minimum_Spanning_Trees/Practise_4_3_03.java
+++ Ch_4_3_Minimum_Spanning_Trees/Practise_4_3_03.java
@@ -1,5 +1,12 @@
 package Ch_4_3_Minimum_Spanning_Trees;
 
 public class Practise_4_3_03 {
-
+    public static void main(String[] args) {
+        /*
+         * 假设最小生成树不唯一，那么假设在 G 中有两个不同的最小生成树 T1 和 T2，再假设 G 中
+         * 有一条最小权重的边 f，该边 f 在 T1 而不在 T2 中，把 f 添加到 T2 中，此时便形成了一个
+         * 闭环，因为在这个闭环中 f 比所有边权重都要小，那么此时删掉其他一条边而保留 f，便形成
+         * 了一个新的最小生成树 T3，此时 T3 == T1
+         */
+    }
 }
---
+++ Ch_4_3_Minimum_Spanning_Trees/Practise_4_3_04.java
@@ -0,0 +1,11 @@
+package Ch_4_3_Minimum_Spanning_Trees;
+
+public class Practise_4_3_04 {
+    public static void main(String[] args) {
+        /*
+         * 如果加权无向图中有相同权重的边，那么假设相同权重的边在整个图中
+         * 具有最小权重，根据 切分定理，权重最小的边必然属于最小生成树，
+         * 那么从不同顶点出发的 Prim 算法必然会得出不同的生成树。
+         */
+    }
+}
