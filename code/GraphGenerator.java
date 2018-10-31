1eae191c3e7dad3ce1cef27a51f5206395f8fb68
yangxiaohei <yangxiaohei321123@163.com>
2018-10-31 21:34:18 +0800

    欧拉路径

--- code/GraphGenerator.java
+++ code/GraphGenerator.java
@@ -15,10 +15,10 @@ public class GraphGenerator {
 //            vertices[i] = StdRandom.uniform(V);
         int[] vertices = ArrayGenerator.parseInts("9    3    7    2    1    7    5    0    5    2  ");
         
-        for (int i = 0; i < E-1; i++) {
-            G.addEdge(vertices[i], vertices[i+1]);
-        }
-        G.addEdge(vertices[E-1], vertices[0]);
+        for (int i = 0; i < E - 1; i++) 
+            G.addEdge(vertices[i], vertices[i + 1]);
+        
+        G.addEdge(vertices[E - 1], vertices[0]);
         StdOut.println("-----------------------");
         for (int i = 0; i < vertices.length; i++)
             StdOut.printf("%-5d", i);
@@ -28,7 +28,21 @@ public class GraphGenerator {
         StdOut.println("\n-----------------------");
         return G;
     }
+    public static Graph eulerianPath(int V, int E) {
+        if (E < 0)
+            throw new IllegalArgumentException("");
+        if (V <= 0)
+            throw new IllegalArgumentException("");
+        Graph g = new Graph(V);
+        int[] vertices = new int[E + 1];
+        for (int i = 0; i < E + 1; i++)
+            vertices[i] = StdRandom.uniform(V);
+        for (int i = 0; i < E; i++)
+            g.addEdge(vertices[i], vertices[i + 1]);
+        return g;
+    }
     public static void main(String[] args) {
-        StdOut.println(GraphGenerator.eulerianCycle(5, 10));
+        StdOut.println(GraphGenerator.eulerianCycle(10, 10));
+        StdOut.println(GraphGenerator.eulerianPath(5, 10));
     }
 }
