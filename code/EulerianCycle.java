bd9f2e4f5f2e2f5d6607fd2de18be69fd0c94ded
yangxiaohei <yangxiaohei321123@163.com>
2018-11-02 10:36:12 +0800

    欧拉环

--- code/EulerianCycle.java
+++ code/EulerianCycle.java
@@ -3,132 +3,140 @@ package code;
 import edu.princeton.cs.algs4.StdOut;
 
 public class EulerianCycle {
-    private _Stack<Integer> cycle = new _Stack<Integer>();  // Eulerian cycle; null if no such cycle
-
-    // an undirected edge, with a field to indicate whether the edge has already been used
+    private Graph g;
+    /*
+     * 之所以要定义该 Edge 数据结构，因为下面需要以边为单位（而非以顶点为单位）
+     * 进行 dfs ，目的是 "一笔画"。之所以要以边为单位进行 dfs 是因为
+     * 一笔画可能需要穿越已经被访问的顶点
+     */
     private static class Edge {
         private final int v;
         private final int w;
-        private boolean isUsed;
-        private static int counter = 0;
+        private static int counter;
         private final int id = counter++;
-
-        public Edge(int v, int w) {
-            this.v = v;
-            this.w = w;
-            isUsed = false;
+        private boolean isUsed;
+        public Edge(int v, int w) { this.v = v; this.w = w; }
+        public int other(int t) { 
+            if (t == v) return w;
+            if (t == w) return v;
+            throw new IllegalArgumentException("v must between " + v + " and " + w);
         }
-        
         public String toString() { return String.format("{%d %d}-%d ", v, w, id); }
-
-        // returns the other vertex of the edge
-        public int other(int vertex) {
-            if      (vertex == v) return w;
-            else if (vertex == w) return v;
-            else throw new IllegalArgumentException("Illegal endpoint");
-        }
     }
-
-    /**
-     * Computes an Eulerian cycle in the specified graph, if one exists.
-     * 
-     * @param G the graph
-     */
-    public EulerianCycle(Graph G) {
-
-        // must have at least one edge
-        if (G.E() == 0) return;
-
-        // necessary condition: all vertices have even degree
-        // (this test is needed or it might find an Eulerian path instead of cycle)
-        for (int v = 0; v < G.V(); v++) 
-            if (G.degree(v) % 2 != 0)
-                return;
-
-        // create local view of adjacency lists, to iterate one vertex at a time
-        // the helper Edge data type is used to avoid exploring both copies of an edge v-w
-        _Queue<Edge>[] adj = (_Queue<Edge>[]) new _Queue[G.V()];
-        for (int v = 0; v < G.V(); v++)
-            adj[v] = new _Queue<Edge>();
-
-        for (int v = 0; v < G.V(); v++) {
-            int selfLoops = 0;
-            for (int w : G.adj(v)) {
-                // careful with self loops
-                if (v == w) {
-                    if (selfLoops % 2 == 0) {
-                        Edge e = new Edge(v, w);
-                        adj[v].enqueue(e);
-                        adj[w].enqueue(e);
-                    }
-                    selfLoops++;
-                }
-                else if (v < w) {
-                    Edge e = new Edge(v, w);
-                    StdOut.printf("[%d %d]\n", v, w);
-                    adj[v].enqueue(e);
-                    adj[w].enqueue(e);
-                }
-            }
-        }
-        
-        StdOut.println("----- Queue -----");
-        for (int i = 0; i < adj.length; i++)
-            StdOut.printf("%3d : %s\n", i, adj[i]);
-        StdOut.println("-----------------");
-
-        // initialize stack with any non-isolated vertex
-        int s = nonIsolatedVertex(G);
-        _Stack<Integer> stack = new _Stack<Integer>();
-        stack.push(s);
-
-        // greedily search through edges in iterative DFS style
-        cycle = new _Stack<Integer>();
-        while (!stack.isEmpty()) {
-            int v = stack.pop();
-            while (!adj[v].isEmpty()) {
-                Edge edge = adj[v].dequeue();
-                StdOut.printf("v = %d edge = %s\n", v, edge);
-                if (edge.isUsed) continue;
-                edge.isUsed = true;
-                stack.push(v);
-                StdOut.printf("push %d to stack\n", v);
-                v = edge.other(v);
-            }
-            StdOut.printf("----- Stack v = %d-----\n", v);
-            StdOut.println(stack);
-            StdOut.println("-----------------");
-            
-            // push vertex with no more leaving edges to cycle
-            cycle.push(v);
-        }
-
-        // check if all edges are used
-        if (cycle.size() != G.E() + 1)
-            cycle = null;
+    private _Stack<Integer> cycle;
+    public EulerianCycle(Graph g) {
+       this.g = g;
+       cycle = new _Stack<>();
+       
+       /*
+        * 必须对顶点进行度数的检查，否则欧拉路径会被识别成欧拉环
+        * 比如欧拉路径 1 - 2 - 3 - 4 会成功被添加到 cycle 路径中
+        * 而我们需要的是欧拉环比如 1 - 2 - 3 - 4 - 1
+        */
+       for (int i = 0; i < g.V(); i++)
+           if (g.degree(i) % 2 != 0)
+               return;
+       
+       /*
+        * 构造一个以 Edge 对象为邻接表以便能以边为单位进行 dfs
+        */
+       @SuppressWarnings("unchecked")
+       _Queue<Edge> adjs[] = (_Queue<Edge>[])new _Queue[g.V()];
+       for (int i = 0; i < g.V(); i++)
+           adjs[i] = new _Queue<>();
+       int selfLoop = 0;
+       for (int i = 0; i < g.V(); i++) {
+           for (int w : g.adj(i)) {
+               if (w == i) {
+                   if (selfLoop % 2 == 0) {
+                       Edge e = new Edge(i, i);
+                       adjs[i].enqueue(e);
+                       adjs[i].enqueue(e);
+                   }
+                   selfLoop++;
+               } else if (w > i) {
+                   Edge e = new Edge(w, i);
+                   adjs[w].enqueue(e);
+                   adjs[i].enqueue(e);
+               }
+           }
+       }
+              
+       StdOut.println("--------- Queue -------");
+       for (int i = 0; i < adjs.length; i++)
+           StdOut.printf("%-2d : %s\n",i, adjs[i]);
+       StdOut.println("---------------------");
+       
+       int nonisolate = 0;
+       for (int i = 0; i < g.V(); i++) {
+           if (g.degree(i) != 0) {
+               nonisolate = i;
+               break;
+           }
+       }
+       StdOut.printf("nonisolate vertex = %d\n\n", nonisolate);
+               
+       /*
+        * 以边为单位进行 dfs，但是记录沿途经历的顶点
+        */
+       _Stack<Integer> S = new _Stack<>();
+       S.push(nonisolate);
+       while (!S.isEmpty()) {
+           int from = S.pop();
+           /*
+            * 第一次跳出循环时添加进来的
+            * 是一笔画的最后一个顶点，但沿途可能有
+            * 没有走的分支，所以一步步后退，若有先前
+            * 忽略的分支，那么就进去
+            */
+           while (!adjs[from].isEmpty()) {
+               Edge e = adjs[from].dequeue();
+               /*
+                *  走过的边直接把它删掉，要注意
+                *  无向边需要删掉两个方向，因为
+                *  添加时是同一个引用，所以直接
+                *  用 isUsed 来判断
+                */
+               if (e.isUsed) continue;
+               e.isUsed = true;
+               S.push(from);
+               from = e.other(from);
+           }
+           /*
+            * 放进 cycle 的顺序依次是一笔画经历的
+            * 倒数第一个顶点，倒数第二个，倒数第三个
+            * ...
+            */
+           StdOut.printf("cycle constructed by %d\n", from);
+           cycle.push(from);
+       }
+       
+       /*
+        * 把一笔画摊开
+        * 
+        * 1 - 2 - 1 - 3 - 4 - 5 - 4 - 7 - 9
+        * 
+        * 因此连接 V 顶点至少需要 E - 1 条边
+        */
+       if (cycle.size() != g.E() + 1) {
+           cycle = null;
+           StdOut.printf("cannot go through all vertices by a nonduplicate path");
+       }
     }
-
     public Iterable<Integer> cycle() {
+        if (!hasEulerianCycle())
+            throw new RuntimeException("eulerianCycle not exist");
         return cycle;
     }
-
     public boolean hasEulerianCycle() {
         return cycle != null;
-    }
-
-    // returns any non-isolated vertex; -1 if no such vertex
-    private static int nonIsolatedVertex(Graph G) {
-        for (int v = 0; v < G.V(); v++)
-            if (G.degree(v) > 0)
-                return v;
-        return -1;
     }
     public static void main(String[] args) {
         Graph g = GraphGenerator.eulerianCycle(10, 10);
         StdOut.print(g);
-        EulerianCycle e = new EulerianCycle(g);
-        if (e.hasEulerianCycle())
-            for (int w : e.cycle())
-                StdOut.print(w + " ");
+        EulerianCycle c = new EulerianCycle(g);
+        if (c.hasEulerianCycle())
+            for (int w : c.cycle())
+                StdOut.println(w);
     }
 }
