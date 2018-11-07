45b5f61050706ad0502a50ea21e0b30ccdb8a6b2
yangxiaohei <yangxiaohei321123@163.com>
2018-11-07 10:21:01 +0800

    tarjanscc 按自己的理解写一遍

--- Review/TarjanSCC.java
+++ Review/TarjanSCC.java
@@ -3,101 +3,102 @@ package Review;
 import edu.princeton.cs.algs4.StdOut;
 
 public class TarjanSCC {
-
-    private boolean[] marked;        // marked[v] = has v been visited?
-    private int[] id;                // id[v] = id of strong component containing v
-    private int[] low;               // low[v] = low number of v
-    private int pre;                 // preorder number counter
-    private int count;               // number of strongly-connected components
-    private _Stack<Integer> stack;
-
-    public TarjanSCC(Digraph G) {
-        marked = new boolean[G.V()];
-        stack = new _Stack<Integer>();
-        id = new int[G.V()]; 
-        low = new int[G.V()];
-        for (int v = 0; v < G.V(); v++) 
-            if (!marked[v]) 
-                dfs(G, v);
-        assert check(G);
+    private boolean[] marked;
+    private int[] low;
+    private Digraph g;
+    private int pre;
+    private int count;
+    private int[] id;
+    private _Stack<Integer> S;
+    public TarjanSCC(Digraph g) {
+        this.g = g;
+        id = new int[g.V()];
+        marked = new boolean[g.V()];
+        low = new int[g.V()];
+        S = new _Stack<>();
+        for (int i = 0; i < g.V(); i++)
+            if (!marked[i])
+                dfs(i);
     }
-
-    private void dfs(Digraph G, int v) { 
+    public int count() { return count; }
+    public boolean areStronglyConnected(int v, int w) { checkVertex(v, w); return id[v] == id[w]; }
+    public int id(int v) { checkVertex(v); return id[v]; }
+    private void checkVertex(int ...v) {
+        for (int i = 0; i < v.length; i++)
+            if (v[i] < 0 || v[i] >= g.V())
+                throw new IllegalArgumentException(String.format("vertex must between %d and %d", 0, g.V() - 1));
+    }
+    private void dfs(int v) {
         marked[v] = true;
-        low[v] = pre++;
-        int min = low[v];
-        stack.push(v);
-        StdOut.printf("%d 入栈，min = %d 打印栈: %s\n", v, min, stack);
-        for (int w : G.adj(v)) {
-            if (!marked[w]) 
-                dfs(G, w);
-            StdOut.printf("邻边 w = %d\n", w);
-            // min = MIN(low[w], min)
-            if (low[w] < min) {
-                StdOut.printf("min 修改为 %d\n", low[w]);
+        int min = pre++;  /* 时间戳递增 */
+        low[v] = min;
+        /*
+         * 用栈来保留依次访问的节点，便于回溯时弹出作为
+         * 一组强连通分量中的节点
+         */
+        S.push(v);
+        for (int w : g.adj(v)) {
+            if (!marked[w])
+                dfs(w);
+            /*
+             * 遍历邻边时发现一个已标记的顶点，其实并不能说明
+             * 发现了环，比如
+             *     a
+             *     .
+             *  b . . c
+             *  
+             * 邻接表为
+             *   a : b
+             *   a : c
+             *   b : c
+             *   
+             *  a -> c，然后 c 返回，a -> b, b -> c 发现 c 已经被标记
+             *  然而此时并不构成一个环，b -> c 这条边称为横叉边，
+             *  而只有后向边能构成环
+             *  
+             *  遇到已标记顶点时采取的措施是，判断该已标记顶点
+             *  是否有后向边，随着递归的深入，递增的时间戳本来不可能
+             *  大于 low[w]，若出现该情况，说明邻接点必然有回向边，
+             *  那么将该层的时间戳置为搜索树中辈分最大的祖宗顶点的时间戳
+             */
+            if (low[w] < min)
                 min = low[w];
-            } 
         }
-        
-        // low[v] = MIN(low[v], min)
+        /*
+         *  访问了一圈邻接点，时间戳居然变小了，说明此刻正在回溯
+         */
         if (min < low[v]) {
-            StdOut.printf("跳出遍历邻边的循环，此时 low[%d] 从 %d 修改为 %d\n",v, low[v], min);
             low[v] = min;
             return;
-        } 
-        
+        }
+        /*
+         * 执行到这里，说明回溯到了 min == low[v] 的位置
+         * 该位置是环的 “起点”，一个强连通分量的分界线
+         */
         int w;
         do {
-            w = stack.pop();
-            StdOut.printf("pop:%d\n", w);
+            w = S.pop();
+            /*
+             * 这一句代码很重要，这样赋值是为了
+             * 避免强连通分量 A 的顶点在访问强连通分量 B
+             * 被标记的顶点时，错把 B 中被访问的顶点纳入
+             * A 中的错误，因为这样赋值后，
+             * 
+             * if (low[w] < min)
+             *   min = low[w];
+             * 
+             *  上面的判断条件永远不会满足
+             *
+             */
+            low[w] = g.V(); 
             id[w] = count;
-            low[w] = G.V();
-            StdOut.printf("low[%d] 修改为 %d\n", w, G.V());
         } while (w != v);
         count++;
     }
-    public int count() {
-        return count;
-    }
-    public boolean stronglyConnected(int v, int w) {
-        validateVertex(v);
-        validateVertex(w);
-        return id[v] == id[w];
-    }
-    public int id(int v) {
-        validateVertex(v);
-        return id[v];
-    }
-    private boolean check(Digraph G) {
-        TransitiveClosure tc = new TransitiveClosure(G);
-        for (int v = 0; v < G.V(); v++) {
-            for (int w = 0; w < G.V(); w++) {
-                if (stronglyConnected(v, w) != (tc.reachable(v, w) && tc.reachable(w, v)))
-                    return false;
-            }
-        }
-        return true;
-    }
-    private void validateVertex(int v) {
-        int V = marked.length;
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
-    }
     public static void main(String[] args) {
-//        Digraph g = DigraphGenerator.normal(8, 10);
-//        Digraph g = new Digraph("{ 4 5 } { 5 7 } { 7 0 } { 7 8 } { 3 8 } { 7 1 } { 8 0 } "
-//                              + "{ 2 1 } { 6 7 } { 5 7 } { 0 6 } { 3 7 } { 5 2 } { 3 6 } "
-//                              + "{ 6 8 } { 1 3 } { 1 7 } { 9 2 } { 3 2 } { 8 6 } { 4 1 } "
-//                              + "{ 4 0 } { 9 9 } { 3 0 } { 2 4 } { 5 0 } { 5 7 } { 3 7 } "
-//                              + "{ 6 0 } { 7 8 }");
-//        Digraph g = new Digraph("{ 6 5 } { 3 6 } { 0 6 } { 6 0 } { 2 6 } "
-//                              + "{ 4 3 } { 7 2 } { 7 2 } { 3 5 } { 5 4 } ");
-        Digraph g = new Digraph("{ 5 3 } { 4 1 } { 6 5 } { 0 7 } { 3 0 }"
-                             + " { 1 6 } { 3 5 } { 3 6 } { 6 4 } { 4 6 }"); 
-        // 3 连通分量: { 5 4 } { 0 6 } { 3 5 } { 6 7 } { 2 4 } { 5 1 } { 1 0 } { 3 6 } { 6 5 } { 4 3 } 
-        // 4 连通分量: { 7 1 } { 0 7 } { 0 0 } { 6 4 } { 1 6 } { 7 6 } { 3 0 } { 1 7 } { 6 0 } { 0 3 } 
+        Digraph g = new Digraph("{0 7} { 1 6} {3  6} {3 5} { 3 0} {4 6} { 4 1} {5 3} { 6 4} { 6 5}");
         StdOut.println(g);
         TarjanSCC scc = new TarjanSCC(g);
-        StdOut.print("强连通分量的个数: " + scc.count());
+        StdOut.println(scc.count());
     }
 }
