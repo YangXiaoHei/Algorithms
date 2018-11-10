ea4de4d488383c7af62e4db534dca0284f10cdb3
yangxiaohei <yangxiaohei321123@163.com>
2018-11-10 19:40:17 +0800

    有向加权图

---
+++ Review/DirectedEdge.java
@@ -0,0 +1,13 @@
+package Review;
+
+public class DirectedEdge {
+    private final int v;
+    private final int w;
+    private final double weight;
+    public DirectedEdge(int v, int w, double weight) { this.v = v; this.w = w; this.weight = weight; }
+    public String toString() {
+        return String.format("{ %d -> %d %.0f }", v, w, weight);
+    }
+    public int from() { return v; }
+    public int to() { return w; }
+}
